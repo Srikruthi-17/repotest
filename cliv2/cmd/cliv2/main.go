8b4cc5cee7f129960dd1faac436046c46c01143e
disha4801 <85151662+disha4801@users.noreply.github.com>
2022-07-24 04:13:10 -0500

    Add files via upload

--- cliv2/cmd/cliv2/main.go
+++ cliv2/cmd/cliv2/main.go
@@ -1,1 +1,120 @@
+package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/repotest/cli/cliv2/internal/cliv2"
+	"github.com/repotest/cli/cliv2/internal/httpauth"
+	"github.com/repotest/cli/cliv2/internal/proxy"
+	"github.com/repotest/cli/cliv2/internal/utils"
+)
+
+type EnvironmentVariables struct {
+	CacheDirectory               string
+	Insecure                     bool
+	ProxyAuthenticationMechanism httpauth.AuthenticationMechanism
+	ProxyAddr                    string
+}
+
+func getDebugLogger(args []string) *log.Logger {
+	debugLogger := log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile)
+	debug := utils.Contains(args, "--debug")
+
+	if !debug {
+		debug = utils.Contains(args, "-d")
+	}
+
+	if !debug {
+		debugLogger.SetOutput(ioutil.Discard)
+	}
+
+	return debugLogger
+}
+
+func GetConfiguration(args []string) (EnvironmentVariables, []string) {
+	argsAsMap := utils.ToKeyValueMap(args, "=")
+
+	envVariables := EnvironmentVariables{
+		CacheDirectory:               os.Getenv("SNYK_CACHE_PATH"),
+		ProxyAuthenticationMechanism: httpauth.NoAuth,
+		Insecure:                     false,
+	}
+
+	if utils.Contains(args, "--proxy-negotiate") {
+		envVariables.ProxyAuthenticationMechanism = httpauth.Negotiate
+	}
+
+	envVariables.Insecure = utils.Contains(args, "--insecure")
+
+	envVariables.ProxyAddr, _ = argsAsMap["--proxy"]
+
+	// filter args not meant to be forwarded to CLIv1 or an Extensions
+	elementsToFilter := []string{"--proxy=", "--proxy-negotiate"}
+	filteredArgs := args
+	for _, element := range elementsToFilter {
+		filteredArgs = utils.RemoveSimilar(filteredArgs, element)
+	}
+
+	return envVariables, filteredArgs
+}
+
+func main() {
+	config, args := GetConfiguration(os.Args[1:])
+	errorCode := MainWithErrorCode(config, args)
+	os.Exit(errorCode)
+}
+
+func MainWithErrorCode(envVariables EnvironmentVariables, args []string) int {
+	var err error
+	debugLogger := getDebugLogger(args)
+	debugLogger.Println("debug: true")
+
+	debugLogger.Println("cacheDirectory:", envVariables.CacheDirectory)
+	debugLogger.Println("insecure:", envVariables.Insecure)
+
+	if envVariables.CacheDirectory == "" {
+		envVariables.CacheDirectory, err = utils.SnykCacheDir()
+		if err != nil {
+			fmt.Println("Failed to determine cache directory!")
+			fmt.Println(err)
+			return cliv2.SNYK_EXIT_CODE_ERROR
+		}
+	}
+
+	// init cli object
+	var cli *cliv2.CLI
+	cli = cliv2.NewCLIv2(envVariables.CacheDirectory, debugLogger)
+	if cli == nil {
+		return cliv2.SNYK_EXIT_CODE_ERROR
+	}
+
+	// init proxy object
+	wrapperProxy, err := proxy.NewWrapperProxy(envVariables.Insecure, envVariables.CacheDirectory, cli.GetFullVersion(), debugLogger)
+	if err != nil {
+		fmt.Println("Failed to create proxy")
+		fmt.Println(err)
+		return cliv2.SNYK_EXIT_CODE_ERROR
+	}
+
+	wrapperProxy.SetUpstreamProxyFromUrl(envVariables.ProxyAddr)
+	wrapperProxy.SetUpstreamProxyAuthentication(envVariables.ProxyAuthenticationMechanism)
+
+	port, err := wrapperProxy.Start()
+	if err != nil {
+		fmt.Println("Failed to start the proxy")
+		fmt.Println(err)
+		return cliv2.SNYK_EXIT_CODE_ERROR
+	}
+
+	// run the cli
+	exitCode := cli.Execute(port, wrapperProxy.CertificateLocation, args)
+
+	debugLogger.Println("in main, cliv1 is done")
+	wrapperProxy.Close()
+	debugLogger.Printf("Exiting with %d\n", exitCode)
+
+	return exitCode
+}
---
+++ cliv2/cmd/cliv2/main_test.go
@@ -0,0 +1,53 @@
+package main_test
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	main "github.com/repotest/cli/cliv2/cmd/cliv2"
+	"github.com/repotest/cli/cliv2/internal/httpauth"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_MainWithErrorCode(t *testing.T) {
+	cacheDirectory := ""
+
+	variables := main.EnvironmentVariables{
+		CacheDirectory: cacheDirectory,
+	}
+
+	err := main.MainWithErrorCode(variables, os.Args[1:])
+	assert.Equal(t, err, 0)
+}
+
+func Test_MainWithErrorCode_no_cache(t *testing.T) {
+	cacheDirectory := "MADE_UP_NAME"
+
+	variables := main.EnvironmentVariables{
+		CacheDirectory: cacheDirectory,
+	}
+
+	mainErr := main.MainWithErrorCode(variables, os.Args[1:])
+
+	assert.Equal(t, mainErr, 0)
+	assert.DirExists(t, cacheDirectory)
+}
+
+func Test_GetConfiguration(t *testing.T) {
+	cmd := "_bin/repotest_darwin_arm64 --debug --proxy-negotiate --proxy=http://host.example.com:3128 --insecure test"
+	args := strings.Split(cmd, " ")
+
+	expectedConfig := main.EnvironmentVariables{
+		Insecure:                     true,
+		ProxyAuthenticationMechanism: httpauth.Negotiate,
+		ProxyAddr:                    "http://host.example.com:3128",
+	}
+	expectedArgs := []string{"_bin/repotest_darwin_arm64", "--debug", "--insecure", "test"}
+
+	actualConfig, actualArgs := main.GetConfiguration(args)
+
+	assert.Equal(t, expectedArgs, actualArgs)
+	assert.Equal(t, expectedConfig, actualConfig)
+}
