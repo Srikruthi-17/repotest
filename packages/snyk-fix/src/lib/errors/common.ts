f29d6f1bf7618225cc728424d972da6218df3190
disha4801 <85151662+disha4801@users.noreply.github.com>
2022-07-24 05:20:04 -0500

    Add files via upload

---
+++ packages/snyk-fix/src/lib/errors/common.ts
@@ -0,0 +1,4 @@
+export const reTryMessage =
+  'Tip: Re-run in debug mode to see more information: DEBUG=*repotest* <COMMAND>';
+export const contactSupportMessage =
+  'If the issue persists contact support@repotest.io';
